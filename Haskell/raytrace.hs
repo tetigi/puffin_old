34b98f6dc874fe91712afecc88f2283c70f1533c
tetigi <luke.tomlin@gmail.com>
2012-10-24 11:44:49 +0100

    Hacked together some matrix functions and the inverse functions. Can definitely be made faster.

--- Haskell/raytrace.hs
+++ Haskell/raytrace.hs
@@ -24,7 +24,14 @@ instance Num a => Num (Vector a) where
   signum _ = 1
 
 vectorDot :: Vector Double -> Vector Double -> Double
-vectorDot (Vector v1) (Vector v2) = sum $ zipWith (*) v1 v2 
+vectorDot (Vector v1) (Vector v2) = sum $ zipWith (*) v1 v2
+
+vectorAppend :: Vector a -> a -> Vector a
+vectorAppend (Vector v) x = Vector (v ++ [x])
+
+-- negates a 4 length vector, graphics styleeeee
+negVector :: (Num a) => Vector a -> Vector a
+negVector (Vector v) = Vector $ (map negate (init v)) ++ [last v]
 
 -- -------------------------------
 -- Matrix
@@ -42,15 +49,39 @@ instance Num a => Num (Matrix a) where
   signum _ = 1
 
 matrixInvertSimple :: Matrix Double -> Matrix Double
-matrixInvertSimple a = undefined
+matrixInvertSimple a = addColumn invTranslation $ trimTo 3 4 blankTranslation
+  where
+    oldScale = getRow 3 $ trimTo 3 4 a
+    translation = getColumn 3 a
+    threeTranspose = addColumn translation $ addRow oldScale $ matrixTranspose $ trimTo 3 3 a
+    blankTranslation = addColumn (Vector [0,0,0,1]) $ trimTo 3 4 threeTranspose
+    invTranslation = matMultVector blankTranslation $ negVector translation
 
 matrixTranspose :: Matrix a -> Matrix a
 matrixTranspose (Matrix as) = Matrix (transpose as)
 
+matMultVector :: Matrix Double -> Vector Double -> Vector Double
+matMultVector (Matrix ms) v = Vector [vectorDot v (Vector m) | m <- ms]
+
 transpose :: [[a]] -> [[a]]
-transpose as
+transpose as 
   | (length . head) as == 0 = []
   | otherwise     = (map head as) : (transpose (map tail as))
+
+trimTo :: Int -> Int -> Matrix a -> Matrix a
+trimTo x y (Matrix as) = Matrix $ map (take x) $ take y as 
+
+addRow :: Vector a -> Matrix a -> Matrix a
+addRow (Vector v) (Matrix as) = Matrix (as ++ [v])
+
+addColumn :: Vector a -> Matrix a -> Matrix a
+addColumn v m = matrixTranspose $ addRow v $ matrixTranspose m
+
+getRow :: Int -> Matrix a -> Vector a
+getRow i (Matrix as) = Vector (as !! i)
+
+getColumn :: Int -> Matrix a -> Vector a
+getColumn i m = getRow i $ matrixTranspose m
 
 --matrixGetTranslate :: Matrix -> Vector
 --matrixGetTranslate a = undefined
